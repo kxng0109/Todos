afe9bd8c5fa274afab3ed18d6ee6034c0787cbd8
kxng0109 <iluhjosh13@gmail.com>
2022-03-22 02:06:56 +0100

    Added some actions, need to check on them again.

--- public/index.js
+++ public/index.js
@@ -1,24 +1,43 @@
 const SLIDER = document.querySelector('#slider');
 const SLIDER_CIRCLE = document.querySelector('#slider-circle');
+const middleText = document.querySelector('#middle-text');
+const deleteDone = document.querySelectorAll('.done-delete');
+const deleteUndone = document.querySelectorAll('.undone-delete');
+const undoneTodos = document.querySelectorAll('.undone-todos');
+const doneTodos = document.querySelectorAll('.done-todos');
+const undoneTodosDiv = document.querySelector('#undone-todos-div');
+const doneTodosDiv = document.querySelector('#done-todos-div');
+
+if (!localStorage.new) {
+	localStorage.setItem('new', 1);
+	middleText.textContent = `Welcome, click the button at the button-right corner to start`;
+}
 
 
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark');
+	document.documentElement.classList.add('dark');
 } else {
-  document.documentElement.classList.remove('dark');
+	document.documentElement.classList.remove('dark');
 }
 
 SLIDER.onclick = () =>{
-  if (document.documentElement.classList.contains('dark')) {
-    localStorage.setItem('theme', 'light');
-    console.log(localStorage.getItem('theme'));
-    return document.documentElement.classList.remove('dark');
+	if (document.documentElement.classList.contains('dark')) {
+	 	localStorage.setItem('theme', 'light');
+	 	return document.documentElement.classList.remove('dark');
   }
 
-  document.documentElement.classList.add('dark');
-  localStorage.setItem('theme', 'dark');
-  console.log(localStorage.getItem('theme'));
+	document.documentElement.classList.add('dark');
+	localStorage.setItem('theme', 'dark');
 }
+
+function deleteAction (theNode) {
+	// console.log(theNode.parentNode)
+	setTimeout(() => theNode.parentNode.removeChild(theNode), 600);
+}
+
+deleteDone.forEach((element, index) =>{
+	deleteDone[index].onclick  = () => deleteAction(doneTodos[index]);
+});
 
 // let themeChecker = () =>{
 //   if (!(localStorage.getItem('theme'))) {
