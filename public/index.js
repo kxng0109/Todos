d3de7182f723e323429e84fae8e1d3a49de36ef4
kxng0109 <iluhjosh13@gmail.com>
2022-03-25 15:12:38 +0100

    Finished delete, edit and checkbox. Still need to do add and also work on the background of the pop up when you edit.

--- public/index.js
+++ public/index.js
@@ -3,8 +3,8 @@ const addBtn = document.querySelector('#add');
 const SLIDER = document.querySelector('#slider');
 const SLIDER_CIRCLE = document.querySelector('#slider-circle');
 const middleText = document.querySelector('#middle-text');
-const deleteDone = document.querySelectorAll('.done-delete');
-const deleteUndone = document.querySelectorAll('.undone-delete');
+const deleteBtn = document.querySelectorAll('.delete');
+const todos = document.querySelectorAll('.todos');
 const undoneTodos = document.querySelectorAll('.undone-todos');
 const doneTodos = document.querySelectorAll('.done-todos');
 const undoneTodosDiv = document.querySelector('#undone-todos-div');
@@ -12,10 +12,13 @@ const doneTodosDiv = document.querySelector('#done-todos-div');
 const pencilIcon = document.querySelectorAll('.pencil-icon');
 const editPopup = document.querySelector('#editpopup');
 const todosText = document.querySelectorAll('.todos-text');
+const editPopupArea = document.querySelector('#editpopuparea');
+const doneEditing = document.querySelector('#done-editing-btn');
+const checkBoxes = document.querySelectorAll('.check-box');
 
 if (!localStorage.new) {
 	localStorage.setItem('new', 1);
-	middleText.textContent = `Welcome, click the button at the button-right corner to start`;
+	// middleText.textContent = `Welcome, click the button at the button-right corner to start`;
 }
 
 
@@ -38,23 +41,59 @@ SLIDER.onclick = () =>{
 pencilIcon.forEach((element, index) =>{
 	pencilIcon[index].onclick = () =>{
 		editPopup.style.display = 'block';
+		console.log(document.body)
 		main.style.filter = 'brightness(50%)';
 		addBtn.style.filter = 'brightness(50%)';
-		editPopup.textContent = todosText[index].textContent
+		editPopupArea.textContent = todosText[index].textContent;
+		editPopupArea.style.border = 'none'
+
 		main.onmousedown = () => {
 			editPopup.style.display = 'none';
 			main.style.filter = 'brightness(100%)';
 			addBtn.style.filter = 'brightness(100%)';
 		}
-		// editPopup.onenter
+
+		doneEditing.onclick = () =>{
+			if (editPopupArea.textContent == '' || editPopupArea.textContent == ' ' || editPopupArea.textContent == '‎') {return editPopupArea.style.border = '1px solid red'}
+			todosText[index].textContent = editPopupArea.textContent;
+			editPopup.style.display = 'none';
+			main.style.filter = 'brightness(100%)';
+			addBtn.style.filter = 'brightness(100%)';
+		}
 	}
 });
 
-function deleteAction (theNode) {
-	// console.log(theNode.parentNode)
-	setTimeout(() => theNode.parentNode.removeChild(theNode), 600);
-}
+checkBoxes.forEach((element, index) =>{
+	checkBoxes[index].oninput = () =>{
+		if (checkBoxes[index].parentNode.classList.contains('undone-todos')) {
+			todos[index].style.animation = 'goOut 0.2s linear forwards';
+			todos[index].classList.remove('undone-todos');
+			todos[index].classList.add('done-todos');
+			setTimeout(() =>{
+				todos[index].style.animation = 'goOut 0.2s linear reverse forwards';
+				doneTodosDiv.appendChild(todos[index]);
+				setTimeout(() => todos[index].style.animation = '' , 200);
+			}, 200);
+		} else {
+			todos[index].style.animation = 'goOut 0.2s linear forwards';
+			todos[index].classList.remove('done-todos');
+			todos[index].classList.add('undone-todos');
+			setTimeout(() =>{
+				todos[index].style.animation = 'goOut 0.2s linear reverse forwards';
+				undoneTodosDiv.appendChild(todos[index]);
+				setTimeout(() => todos[index].style.animation = '' , 200);
+			}, 200);
+		}
+	}
+})
 
-deleteDone.forEach((element, index) =>{
-	deleteDone[index].onclick  = () => deleteAction(doneTodos[index]);
+deleteBtn.forEach((element, index) =>{
+	deleteBtn[index].onclick  = () => {
+		todos[index].style.animation = 'goOut 0.5s linear forwards';
+		setTimeout(() => todos[index].remove(), 500);
+	};
 });
+
+// function checker(){
+
+// }
